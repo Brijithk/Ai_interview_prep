7dfe374c11dcf7f425169463e166a4cf3d6eab34
Brijithk <keranbrijith@gmail.com>
2026-05-23 21:00:00 +0530

    added login and registration api

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 
 const app = express();
-
+const authRoutes = require("./routes/authRoutes");
 app.use(express.json());
 
 require("dotenv").config();
 
 const connectDB = require("./config/db");
-
+app.use("/api/auth", authRoutes);
 connectDB();
 
 app.get("/", (req, res) => {
